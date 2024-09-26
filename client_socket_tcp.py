b0f47a0d5106c8fe424882269655bcb41bc1a4ce
sousaz <gabrielsousa2003gsa@gmail.com>
2024-09-26 00:47:35 +0000

    socket tcp

---
+++ client_socket_tcp.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+def http_client(host, port, path):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        
+        s.sendall(f"GET /{path} HTTP/1.1\r\nHost: {host}\r\n\r\n".encode('utf-8'))
+        
+        res = b""
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            res += data
+
+        print(res.decode('utf-8'))
+
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <host> <port> <path>")
+    sys.exit(1)
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+path = sys.argv[3]
+
+for i in range(4):
+    http_client(host, port, path)
---
+++ server_socket_tcp.py
@@ -0,0 +1,62 @@
+import socket
+import os
+import sys
+import threading
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+def read_html_file(filepath):
+    if os.path.exists(filepath):
+        with open(filepath, "r", encoding='utf-8') as file:
+            return file.read()
+    else:
+        return None
+
+def build_response(status_code, content_type, content):
+    response_line = f"HTTP/1.1 {status_code}\r\n"
+    headers = f"Content-Type: {content_type}\r\nContenmt-Length: {len(content)}\r\n"
+    return f"{response_line}{headers}\r\n{content}"
+
+def handle_client(conn, addr):
+    with conn:
+        try:
+            # print(f"Connected by {addr}")
+            req = conn.recv(1024).decode('utf-8')
+            # print(f"Received: {req}")
+
+            path = req.splitlines()[0]
+            path = path.split()[1]
+            if path == "/":
+                path = "helloWorld.html"
+            path = path.lstrip("/")
+
+            html_content = read_html_file(path)
+            if html_content is None:
+                res = build_response(404, "text/plain", "404 Not found")
+            else:
+                res = build_response(200, "text/html", html_content)
+
+            conn.sendall(res.encode('utf-8'))
+            # print("Sent response")
+        except Exception as e:
+            print(f"Error: {e}")
+            res = build_response(500, "text/plain", "Internal server error")
+            conn.sendall(res.encode('utf-8'))
+        finally:
+            conn.close()
+            # print("Connection closed")
+            sys.exit(0)
+    
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Server listening on {HOST}:{PORT}")
+        while True:
+            conn, addr = s.accept()
+            # print(f"New connection from {addr}")
+            threading.Thread(target=handle_client, args=(conn, addr)).start()
+
+start_server()
