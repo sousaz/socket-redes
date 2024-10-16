abcab261459fb288d719e66a78d9a42083878544
sousaz <gabrielsousa2003gsa@gmail.com>
2024-10-16 02:49:53 +0000

    .

--- clients/smtp_client.py
+++ clients/smtp_client.py
@@ -19,16 +19,23 @@ def send_file(s, filename):
         s.send(f.encode('utf-8'))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # 25, 465ssl, 587tls 2525
+    #587 é um padrão e suportado por muitos servidores
+    #suporta comunicação com e sem tls
+    #Outras portas são bloqueadas para evitar spam
     s.connect((mailserver, 587))
     recv = s.recv(1024).decode('utf-8')
     print("recv1:", recv)
 
     #Inicia comunicação atraves do comando EHLO
+    #Alice é um nome ficticio, normalmente é o nome do servidor, host, identificador
     send_command(s, "EHLO Alice")
 
     #Inicia o TLS atraves do comando STARTTLS
+    #diz ao servidor para mudar para uma conexão segura, e cria uma camada de transporte segura
     send_command(s, "STARTTLS")
     #Camada SSL
+    #Cria um socket securo que ira criptografar os dados e usar o TLS
     context = ssl.create_default_context()
     secure = context.wrap_socket(s, server_hostname=mailserver)
 
@@ -45,12 +52,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #Destinatario
     send_command(secure, "RCPT TO: <" +username+ ">")
 
-    #Envio de dados
+    #Avisa que vai enviar o corpo do email
     send_command(secure, "DATA")
 
     #Corpo do email
     secure.send("Subject: Teste de envio de email\r\n".encode('utf-8'))
+    #MIME (Multipurpose Internet Mail Extensions) é uma extensão do 
+    #protocolo de e-mail que permite o envio de diferentes tipos de 
+    #conteúdo além de texto simples.
     secure.send("MIME-Version: 1.0\r\n".encode('utf-8'))
+    #boundary é um delimitador que separa as partes do email
+    #usado para demilitar a parte do texto e a parte do arquivo
     secure.send("Content-Type: multipart/mixed; boundary=\"frontier\"\r\n".encode('utf-8'))
     secure.send("\r\n".encode('utf-8'))
 
